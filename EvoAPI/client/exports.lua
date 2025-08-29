73027e96a2c410bc61104dfa7aa5d1c6c2da10a8
EVO Modding <devlooxyz@gmail.com>
2025-08-29 03:35:54 -0500

    Delete EvoAPI directory

--- EvoAPI/client/exports.lua
+++
@@ -1,9 +0,0 @@
-
--- EvoAPI Client Exports
-exports("Notify", function(msg, ntype)
-    TriggerEvent("EvoAPI:Notify", msg, ntype)
-end)
-
-exports("ShowUI", function(title, message)
-    TriggerEvent("EvoAPI:ShowUI", title, message)
-end)
--- EvoAPI/modules/client/example_client.lua
+++
@@ -1,5 +0,0 @@
-
--- EvoAPI Example Client Module
-RegisterCommand("evoapi_ping", function()
-    TriggerServerEvent("EvoAPI:Example:Ping", {})
-end, false)
