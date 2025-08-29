63f45f235984d7c895404a4992195f17a3d8afc4
EVO Modding <devlooxyz@gmail.com>
2025-08-29 16:40:06 -0500

    Delete client directory

--- client/exports.lua
+++
@@ -1,8 +0,0 @@
-
-exports("Notify", function(msg, ntype)
-    TriggerEvent("EvoAPI:Notify", msg, ntype)
-end)
-
-exports("ShowUI", function(title, message)
-    TriggerEvent("EvoAPI:ShowUI", title, message)
-end)
