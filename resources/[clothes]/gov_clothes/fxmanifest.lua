1b2c1f7affb3a58d8d749101be4e36dc6f144f3c
EVO Modding <devlooxyz@gmail.com>
2025-09-01 16:45:30 -0500

    b

---
+++ resources/[clothes]/gov_clothes/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author ''
+description ''
+version ''
+lua54 'yes'
+server_scripts {
+  'version.lua',
+}
+files {
+  'mp_m_freemode_01_onxgovt.meta',
+  'mp_f_freemode_01_onxgovt.meta',
+}
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_onxgovt.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_onxgovt.meta'
